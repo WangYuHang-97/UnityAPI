60441b29e77a2b64b2224fd6c13e8220970448fe
yuhang wang <1031216597@qq.com>
2020-12-28 09:32:28 +0800

    Update 2020-12-28

--- 文件夹/系统总结/事件的监听与广播系统/Common/Callback.cs
+++
@@ -1,6 +0,0 @@
-﻿public delegate void Callback();
-public delegate void Callback<T>(T arg);
-public delegate void Callback<T, X>(T arg1, X arg2);
-public delegate void Callback<T, X, Y>(T arg1, X arg2, Y arg3);
-public delegate void Callback<T, X, Y, Z>(T arg1, X arg2, Y arg3, Z arg4);
-public delegate void Callback<T, X, Y, Z, W>(T arg1, X arg2, Y arg3, Z arg4, W arg5);
